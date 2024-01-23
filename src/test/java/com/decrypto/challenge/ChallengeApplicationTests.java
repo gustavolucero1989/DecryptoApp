9910fe5f7debcc1cc8e0cab5eaccfc84c2fb59e9
gustavolucero1989 <gustavolucero1989@gmail.com>
2024-01-23 02:43:10 -0300

    first commit

---
+++ src/test/java/com/decrypto/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decrypto.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
